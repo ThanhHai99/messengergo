a0b3f5e3666451272a7c1842762cd9bd006c63a1
ThanhHai99 <haitran23031999@gmail.com>
2019-10-16 21:23:29 +0700

    2019/10/16 21:23:29.87

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var app = require("express")()
+const app = require("express")();
 
-var hostname = 'localhost'
-var port = 8017
+const hostname = "localhost";
+const port = 8017;
 
-app.get("helloworld",(req, res)=>res.send("<h1>Hello Thanh Hai !</h1>"))
-app.listen(port, hostname, ()=> console.log(`App running at ${hostname}:${port}`))
+app.get("/helloworld", (req, res) => res.send("<h1>Hello Thanh Hai !</h1>"));
+app.listen(port, hostname, () =>
+  console.log(`App running at ${hostname}:${port}`)
+);
