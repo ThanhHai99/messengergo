d8f1f25ea6a15f6dd9b1680e58f8fe89aeac4e54
ThanhHai99 <haitran23031999@gmail.com>
2020-01-20 17:39:11 +0700

    [1.0.0] Login local account function.

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 let Schema = mongoose.Schema;
 let UserSchema = new Schema({
@@ -47,7 +48,16 @@ UserSchema.statics = {
       {"local.verifyToken": token},
       {"local.isActive": true, "local.verifyToken": null}
     ).exec();
+  },
+  findUserById(id){
+    return this.findById(id).exec();
   }
 };
+
+UserSchema.methods = {
+  comparePassword(password){
+    return bcrypt.compare(password, this.local.password); //return a promise has result is true or false
+  }
+}
 
 module.exports = mongoose.model("user", UserSchema);
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,11 @@
 import express from "express";
 import {auth, home} from "./../controllers/index";
 import {authValid} from "./../validation/index";
+import passport from "passport";
+import initPassportLocal from "./../controllers/passportController/local";
+
+//Init all passport
+initPassportLocal();
 
 let router = express.Router();
 
@@ -14,6 +19,13 @@ let initRoutes = (app) =>{
     router.get("/login-register", auth.getLoginRegister);
     router.post("/register", authValid.register, auth.postRegister);
     router.get("/verify/:token", auth.verifyAccount);
+
+    router.post("/login", passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login-register",
+        successFlash: true,
+        failureFlash: true
+    }));
 
     return app.use("/", router);
 };
