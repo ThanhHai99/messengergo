d58cf2c014ff7ea7f9a9c5589b19d3f21408d77d
ThanhHai99 <haitran23031999@gmail.com>
2020-01-26 13:27:13 +0700

    [1.0.0] Login google account.

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,10 +4,12 @@ import {authValid} from "./../validation/index";
 import passport from "passport";
 import initPassportLocal from "./../controllers/passportController/local";
 import initPassportFacebook from "./../controllers/passportController/facebook";
+import initPassportGoogle from "./../controllers/passportController/google";
 
 //Init all passport
 initPassportLocal();
 initPassportFacebook();
+initPassportGoogle();
 
 let router = express.Router();
 
@@ -28,8 +30,14 @@ let initRoutes = (app) =>{
         failureFlash: true
     }));
 
-    router.get("/auth/facebook", passport.authenticate("facebook", {scope: ["email"]}));
-    router.get("/auth/facebook/callback", passport.authenticate("facebook", {
+    router.get("/auth/facebook",auth.checkLoggedOut, passport.authenticate("facebook", {scope: ["email"]}));
+    router.get("/auth/facebook/callback",auth.checkLoggedOut, passport.authenticate("facebook", {
+        successRedirect: "/",
+        failureRedirect: "/login-register"
+    }));
+
+    router.get("/auth/google",auth.checkLoggedOut, passport.authenticate("google", {scope: ["email"]}));
+    router.get("/auth/google/callback",auth.checkLoggedOut, passport.authenticate("google", {
         successRedirect: "/",
         failureRedirect: "/login-register"
     }));
