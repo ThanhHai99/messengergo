45190e869d1cac2e624fd3ab7e4e4bfb5e0a5932
ThanhHai99 <haitran23031999@gmail.com>
2020-03-10 13:35:56 +0700

    [1.0.0] Get newest message.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 import {notification, contact, message} from "./../services/index";
-import {bufferToBase64} from "./../helpers/clientHelper";
+import {bufferToBase64, lastItemOfArray, convertTimestampToHumanTime} from "./../helpers/clientHelper";
 
 let getHome = async (req, res) => {
 	// Only 10 items one time.
@@ -20,9 +20,6 @@ let getHome = async (req, res) => {
 	let countAllContactsReceived = await contact.countAllContactsReceived(req.user._id);
 
 	let getAllConversationItems = await message.getAllConversationItems(req.user._id);
-	let allConversations = getAllConversationItems.allConversations;
-	let userConversations = getAllConversationItems.userConversations;
-	let groupConversations = getAllConversationItems.groupConversations;
 
 	let allConversationWithMessages = getAllConversationItems.allConversationWithMessages;
 
@@ -38,11 +35,10 @@ let getHome = async (req, res) => {
 		countAllContacts,
 		countAllContactsSent,
 		countAllContactsReceived,
-		allConversations,
-		userConversations,
-		groupConversations,
 		allConversationWithMessages,
-		bufferToBase64
+		bufferToBase64,
+		lastItemOfArray,
+		convertTimestampToHumanTime
 	});
 };
 
--- src/helpers/clientHelper.js
+++ src/helpers/clientHelper.js
@@ -1,6 +1,25 @@
+import moment from "moment";
+
 let bufferToBase64 = (bufferFrom) => {
   return Buffer.from(bufferFrom).toString("base64");
 }
+
+let lastItemOfArray = (array) => {
+  if (!array.length) {
+    return [];
+  }
+  return array[array.length - 1];
+};
+
+let convertTimestampToHumanTime = (timestamp) => {
+  if (!timestamp) {
+    return "";
+  }
+  return moment(timestamp).locale("vi").startOf("seconds").fromNow();
+};
+
 module.exports = {
-  bufferToBase64
+  bufferToBase64,
+  lastItemOfArray,
+  convertTimestampToHumanTime
 };
