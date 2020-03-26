3daaf143a5fbdbfa18d91d387e214de5d3aadba4
ThanhHai99 <haitran23031999@gmail.com>
2020-03-26 11:38:17 +0700

    2020/03/26 11:38:17

--- src/controllers/passportController/facebook.js
+++ src/controllers/passportController/facebook.js
@@ -14,48 +14,46 @@ let fbAppCallbackUrl = process.env.FB_APP_CALLBACK_URL;
  * Valid user account type local
  */
 let initPassportFacebook = () => {
+    /*
     passport.use(new FacebookStratery({
         clientID: fbAppId,
         clientSecret: fbAppSecret,
         callbackURL: fbAppCallbackUrl,
-        // passReqToCallback: true,
-        // profileFields: ["email", "gender", "displayName"]
+        passReqToCallback: true,
+        profileFields: ["email", "gender", "displayName"]
     }, async (req, accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        // try {
-        //     let user = await UserModel.findByFacebookUid(profile.id);
-        //     if(user){
-        //         return done(null, user, req.flash("success", transSuccess.loginSuccess(user.username)));
-        //     }
+        try {
+            let user = await UserModel.findByFacebookUid(profile.id);
+            if(user){
+                return done(null, user, req.flash("success", transSuccess.loginSuccess(user.username)));
+            }
 
-        //     let newUserItem = {
-        //         username: profile.displayName,
-        //         gender: profile.gender,
-        //         local: {isActive: true},
-        //         facebook:{
-        //             uid: profile.id,
-        //             token: accessToken,
-        //             email: profile.emails[0].value
-        //         }
-        //     };
-        //     let newUser = await UserModel.createNew(newUserItem);
-        //     return done(null, newUser, req.flash("success", transSuccess.loginSuccess(newUser.username)));
-        // } catch (error) {
-        //     console.log(error);
-        //     return done(null, false, req.flash("errors", transErrors.server_error));
-        // }
+            let newUserItem = {
+                username: profile.displayName,
+                gender: profile.gender,
+                local: {isActive: true},
+                facebook:{
+                    uid: profile.id,
+                    token: accessToken,
+                    email: profile.emails[0].value
+                }
+            };
+            let newUser = await UserModel.createNew(newUserItem);
+            return done(null, newUser, req.flash("success", transSuccess.loginSuccess(newUser.username)));
+        } catch (error) {
+            console.log(error);
+            return done(null, false, req.flash("errors", transErrors.server_error));
+        }
     }));
     //Save userId to session
     passport.serializeUser((user, done) => {
-        // done(null, user._id);
-        done(null, user.id);
+        done(null, user._id);
     });
 
     passport.deserializeUser(async (id, done) => {
         try {
             let user = await UserModel.findUserByIdForSessionToUse(id);
-            // let getChatGroupIds = await ChatGroupModel.getChatGroupIdsByUser(user._id);
-            let getChatGroupIds = await ChatGroupModel.getChatGroupIdsByUser(user.id);
+            let getChatGroupIds = await ChatGroupModel.getChatGroupIdsByUser(user._id);
       
             user = user.toObject();
             user.chatGroupIds = getChatGroupIds;
@@ -64,6 +62,32 @@ let initPassportFacebook = () => {
             return done(error, null);
           }
     });
+    */
+
+    passport.use(new passportFacebook({
+        // clientID: "498842094127366",
+        // clientSecret: "b3b8079e7a054889099f00ec7a4c36da",
+        // callbackURL: "https://localhost:3000/auth/facebook/callback"
+        clientID: "498842094127366",
+        clientSecret: "b3b8079e7a054889099f00ec7a4c36da",
+        callbackURL: "https://messengergo.herokuapp.com/auth/facebook/callback"
+        
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+    }
+    ))
+
+    passport.serializeUser((user, done) => {
+        console.log('ok');
+        return;
+    })
+
+    passport.deserializeUser((id, done) => {  
+        console.log('ok');
+        return;
+    })
+
 };
 
 module.exports = initPassportFacebook;
