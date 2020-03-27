bb2f77dfa1b5f798e49ffc6d6e77df972e2bb387
ThanhHai99 <haitran23031999@gmail.com>
2020-03-27 22:49:19 +0700

    2020/03/27 22:49:18

--- src/config/app.js
+++ src/config/app.js
@@ -3,7 +3,7 @@ const app = {
   avatar_directory: "src/public/images/users",
   avatar_type: ["image/png", "image/jpg", "image/jpeg"],
   avatar_limit_size: 1048576, //byte = 1MB
-  general_avatar_group_chat: "group-avatar-default.png",
+  general_avatar_group_chat: "group-avatar-default.jpg",
   image_message_directory: "src/public/images/chat/message",
   image_message_type: ["image/png", "image/jpg", "image/jpeg"],
   image_message_limit_size: 1048576, //byte = 1MB
